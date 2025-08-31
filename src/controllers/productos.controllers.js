04a73dbedc7e11a0a3652429ac115d47dbbeb898
Santiago Lacki <santiagolacki@gmail.com>
2025-08-31 01:30:56 -0300

    Se separo la logica de la funcion de prueba y se la agrego al controlador

---
+++ src/controllers/productos.controllers.js
@@ -0,0 +1,8 @@
+export const test = (req, res) => {
+  res.status(200)
+  res.send('Primera prueba desde el backend!!')
+}
+
+// Agregar funcion para crear productos
+
+// Agregar funcion para editar productos
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,10 +1,8 @@
 import { Router } from "express";
+import { test } from "../controllers/productos.controllers.js";
 
 const router = Router()
 
-router.use('/test', (req, res) => {
-  res.status(200)
-  res.send('Primera prueba desde el backend!!')
-})
+router.route('/test').get(test)
 
 export default router;
