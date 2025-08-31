4c507c1f64f12bce2f546e2a9a6ba4863ae99eb0
Santiago Lacki <santiagolacki@gmail.com>
2025-08-31 01:25:00 -0300

    Agregamos una ruta de prueba para productos

---
+++ src/routes/index.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import productoRoutes from "./productos.routes.js";
+
+const router = Router()
+
+router.use('/productos', productoRoutes)
+
+export default router;
